426dc6fd0ea45b93a4fb542a49fa382f7ccfdb49
Meghan Casper <casper.sriracha@gmail.com>
2021-06-29 15:27:20 -0400

    cleaned up comments

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 
 const https = require('https');
-//const username = 'meghancasper';
 
 //Function to print message to console
 function printMessage(username, badgeCount, points) {
@@ -10,14 +9,10 @@ function printMessage(username, badgeCount, points) {
     console.log(message);
 }
 //Test function
-//printMessage('meghancasper', 100, 200000);
 function getProfile(username) {
 
     //Connect to API URL ('https://teamtreehouse.com/username.json')cons
     const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-    //use console.dir to get object properties on https request.
-    //console.dir(repsonse);
-    //console.log(response.statusCode);
     let body = '';
     //Read the data
     //response is in buffer form and toString needs to be called on data
@@ -26,19 +21,12 @@ function getProfile(username) {
     });
 
     response.on('end', () => {
-            //Parse the data
+        //Parse the data
         const profile = JSON.parse(body);
         //Print the data
-        printMessage(username, profile.badges.length, profile.points.JavaScript);
-        //console.dir(profile);
+        printMessage(username, profile.badges.length, profile.points.JavaScript);    });
     });
-});
 }
-
-//getProfile('chalkers');
-//getProfile('meghancasper');
-//condense repetitive code into forEach loop
-//const users = ['chalkers', 'meghancasper'];
 
 //use process to get object and slice out first 2 elements in array to get only relevant indices of the array returned
 //just add username after calling app.js in terminal. will printMessage for each username
