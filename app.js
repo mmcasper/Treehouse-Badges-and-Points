de3c2bac1ce4d8b86e03142fa9f763e4023d5274
Meghan Casper <casper.sriracha@gmail.com>
2021-06-29 15:07:19 -0400

    created getProfule function to access username to get printMessage account

--- app.js
+++ app.js
@@ -2,17 +2,19 @@
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 
 const https = require('https');
-const username = 'meghancasper';
+//const username = 'meghancasper';
 
 //Function to print message to console
 function printMessage(username, badgeCount, points) {
-    const message = `${username} has ${badgeCount} total badge(s) and ${points} in JavaScript`;
+    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
     console.log(message);
 }
 //Test function
 //printMessage('meghancasper', 100, 200000);
-//Connect to API URL ('https://teamtreehouse.com/username.json')cons
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+function getProfile(username) {
+
+    //Connect to API URL ('https://teamtreehouse.com/username.json')cons
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
     //use console.dir to get object properties on https request.
     //console.dir(repsonse);
     //console.log(response.statusCode);
@@ -31,4 +33,6 @@ const request = https.get(`https://teamtreehouse.com/${username}.json`, response
         //console.dir(profile);
     });
 });
-
+}
+getProfile('chalkers');
+getProfile('meghancasper');
