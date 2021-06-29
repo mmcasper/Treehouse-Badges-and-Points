4fb26dee0c36ba9d6e39d4635644756ba0fcf715
Meghan Casper <casper.sriracha@gmail.com>
2021-06-29 15:57:47 -0400

    created printError function and replaced code in error handlers

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // Problem: We need a simple way to look at a user's badge count and JavaScript points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 
+//require https module
 const https = require('https');
+
+//Print Error Messages function
+function printError(error) {
+    console.error(error.message);
+}
 
 //Function to print message to console
 function printMessage(username, badgeCount, points) {
@@ -27,14 +33,14 @@ function getProfile(username) {
                 //Print the data
                 printMessage(username, profile.badges.length, profile.points.JavaScript);   
             } catch (error) {
-                console.error(error.message);
+                printError(error);
             }
             });
         });
         //check for error on https request (ex: misspelled url)
-        request.on('error', error => console.error(`Problem with request: ${errror.message}`));
+        request.on('error', printError);
     } catch (error) {
-        console.error(error.message);
+        printError(error);
     }
 }
 
